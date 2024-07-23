bdea6c2e35fe9faa61601fc3f6006734e453fc49
Esben Jannik Bjerrum <esbenjannik@rocketmai.com>
2024-07-23 17:18:25 +0200

    removed unneeded import 'darkdetect'

--- rdeditor/rdEditor.py
+++ rdeditor/rdEditor.py
@@ -13,7 +13,6 @@ from PySide6 import QtSvg
 from PySide6.QtCore import QUrl
 from PySide6.QtGui import QDesktopServices, QIcon, QAction, QKeySequence
 
-import darkdetect
 import qdarktheme
 
 # Import model
